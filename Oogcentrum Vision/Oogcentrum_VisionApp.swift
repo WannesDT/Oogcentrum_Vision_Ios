766fae1d39587d2bfbcc963f8bf5b322040686e0
Wannes De Tollenaere <wannes.detollenaere@student.hogent.be>
2023-12-30 10:56:16 +0100

    Initial Commit

---
+++ Oogcentrum Vision/Oogcentrum_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oogcentrum_VisionApp.swift
+//  Oogcentrum Vision
+//
+//  Created by Wannes De Tollenaere on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Oogcentrum_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
